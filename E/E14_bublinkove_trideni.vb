7f723c054a931dd2ba62af922349a518de1f8ce0
Miroslav Netik <netik.mira.f@gmail.com>
2023-11-25 16:57:36 +0100

    Doděláno E15 - morseovka.

---
+++ E/E14_bublinkove_trideni.vb
@@ -0,0 +1,72 @@
+﻿Module E14_bublinkove_trideni
+    '(14) Vygenerujte deset celých čísel a setřiďte je.  Použijte např. algoritmus Bublinkového třídění (Bubble sort)
+    Sub Mainx()
+        MsgBox("Jsem E14")
+        Dim pole_cisel() As Long
+        Dim nahodna As Long, porovnavaci_i As Long
+        Dim i As Integer, i_2 As Integer, i_3 As Integer, velikost As Integer
+        Dim txt_nahodna As String, txt_serazena As String
+
+        txt_nahodna = ""
+        txt_serazena = ""
+        velikost = 50
+        ReDim pole_cisel(velikost)
+
+        Randomize()
+
+        For i = 0 To velikost
+            nahodna = Int(Rnd() * (10 - (-10) + 1) + (-10))
+            pole_cisel(i) = nahodna
+            txt_nahodna += Str(nahodna) + ", "
+        Next
+
+        For i = velikost To 0 Step -1
+            For i_2 = 0 To i - 1
+                If pole_cisel(i_2) > pole_cisel(i_2 + 1) Then
+                    porovnavaci_i = pole_cisel(i_2 + 1)
+                    pole_cisel(i_2 + 1) = pole_cisel(i_2)
+                    pole_cisel(i_2) = porovnavaci_i
+                End If
+            Next
+        Next
+
+        For i_3 = 0 To velikost
+            txt_serazena += Str(pole_cisel(i_3)) + ", "
+        Next
+        MsgBox(txt_nahodna + Chr(10) + txt_serazena)
+    End Sub
+
+    Sub Mainx_2()
+        MsgBox("Jsem E 14 - verze ze skript")
+
+        Dim A(100) As Long, n As Long, cislo As Long
+        Dim i As Long, j As Long, p As Long
+        Dim s As String
+
+        n = 0
+        i = 0
+        Do
+            cislo = InputBox("Zadej číslo (0 končí): ")
+            If cislo <> 0 Then
+                n = n + 1
+                A(n) = cislo
+            End If
+        Loop Until cislo = 0
+
+        For j = 0 To n - 1
+            For i = 0 To n - 1
+                If A(i) > A(i + 1) Then
+                    p = A(i)
+                    A(i) = A(i + 1)
+                    A(i + 1) = p
+                End If
+            Next
+        Next
+
+        For i = 0 To n
+            s += Chr(10) + Str(A(i))
+        Next
+
+        MsgBox(s)
+    End Sub
+End Module
---
+++ E/E15_Slova_v_Morseovce.vb
@@ -0,0 +1,60 @@
+﻿Module E15_Slova_v_Morseovce
+    '15) Uživatel zadá několik slov (bez diakritiky). Program zapíše tento text v Morseově abecedě.
+    'Oddělovačem znaků bude lomítko. zadat všechna slova do jednoho inputboxu, oddělená normálně mezerami.
+    'A mezera se překládá taky jako lomítko. Tedy na hranicích slov budou vlastně dvě lomítka.- (Nutná znalost Morseovy abecedy). 
+    Sub Mainx()
+        MsgBox("Jsem E15")
+        Dim pole_morseovky(25) As String
+        Dim i As Integer
+        Dim txt_vystup As String, txt_zadani As String, zadane_pismeno As String
+        Dim prelozene_pismeno As String
+
+        pole_morseovky(0) = ".-" ' A
+        pole_morseovky(1) = "-..." ' B
+        pole_morseovky(2) = "-.-." ' C
+        pole_morseovky(3) = "-.." 'D
+        pole_morseovky(4) = "." 'E
+        pole_morseovky(5) = "..-." 'F
+        pole_morseovky(6) = "--." 'G
+        pole_morseovky(7) = "...." 'H
+        pole_morseovky(8) = ".." 'I
+        pole_morseovky(9) = ".---" 'J
+        pole_morseovky(10) = "-.-" 'K
+        pole_morseovky(11) = ".-.." 'L
+        pole_morseovky(12) = "--" 'M
+        pole_morseovky(13) = "-." 'N
+        pole_morseovky(14) = "---" 'O
+        pole_morseovky(15) = ".--." 'P
+        pole_morseovky(16) = "--.-" 'Q
+        pole_morseovky(17) = ".-." 'R
+        pole_morseovky(18) = "..." 'S
+        pole_morseovky(19) = "-" 'T
+        pole_morseovky(20) = "..-" 'U
+        pole_morseovky(21) = "...-" 'V
+        pole_morseovky(22) = ".--" 'W
+        pole_morseovky(23) = "-..-" 'X
+        pole_morseovky(24) = "-.--" 'Y
+        pole_morseovky(25) = "--.." 'Z
+
+        prelozene_pismeno = ""
+        txt_vystup = ""
+
+        txt_zadani = InputBox("Zadej text pro převod do Moreovky: ")
+
+        txt_zadani = UCase(txt_zadani) 'Převede všechno na velká písmena, morseovce je to jedno
+        'Rozdělení slova na znaky oddělené "/" a nahrazení mezery za "/"
+        For i = 1 To Len(txt_zadani)
+            zadane_pismeno = Mid(txt_zadani, i, 1)
+            If zadane_pismeno = " " Then
+                prelozene_pismeno = "/"
+            Else
+                'Platí pro velká písmena, pro malá, pokud bych je nechal by bylo -97
+                prelozene_pismeno = pole_morseovky(Asc(zadane_pismeno) - 65) + "/"
+            End If
+            txt_vystup += prelozene_pismeno
+
+        Next
+
+        MsgBox(txt_vystup)
+    End Sub
+End Module
